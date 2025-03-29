a0e5c5fcde8dd605f796f6146a66d987acdac8fd
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2025-03-29 14:01:43 -0400

    temp

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -56,6 +56,6 @@ export const signUp = async (req, res, next) => {
   }
 };
 
-export const signIn = async (req, res, next) => {};
+// export const signIn = async (req, res, next) => {};
 
-export const signOut = async (req, res, next) => {};
+// export const signOut = async (req, res, next) => {};

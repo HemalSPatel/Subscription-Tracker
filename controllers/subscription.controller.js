9da65c42b6f07c4daffb918e56365f29a59d308c
Hemal Patel <95436169+HemalSPatel@users.noreply.github.com>
2025-04-06 21:35:04 -0400

    impl fetching all subscriptions for a user

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -7,13 +7,32 @@ export const createSubscription = async (req, res, next) => {
       user: req.user._id,
     });
 
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "Subscription created successfully.",
-        data: subscription,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "Subscription created successfully.",
+      data: subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    if (req.user.id !== req.params.id) {
+      const error = new Error(
+        "You are not authorized to access this resource.",
+      );
+      error.statusCode = 403;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({
+      success: true,
+      data: subscriptions,
+    });
   } catch (error) {
     next(error);
   }
